4aa1151c639cc494a0536f47919804ce3e1e99f4
mtayyabpro <mtayyabpro@gmail.com>
2023-06-05 22:03:44 +0500

    updated

--- lib/screens/track_record.dart
+++ lib/screens/track_record.dart
@@ -20,6 +20,36 @@ class SecondPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return
+      Container(
+        color: Colors.green,
+        child: Center(
+          child: Text(
+            'Second Page',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+        ),
+      );
+  }
+}
+class thirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
+      Container(
+        color: Colors.green,
+        child: Center(
+          child: Text(
+            'Second Page',
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+        ),
+      );
+  }
+}
+class fourthPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return
       ListView(
         children: [
           SizedBox(height: 30,),
@@ -41,10 +71,10 @@ class SecondPage extends StatelessWidget {
                 ),
                 child: Container(
 
-                  child: ListTile(
-                    title: Text("All"),
-                    trailing: Image.asset('assets/icons/Polygon 1.png',scale: 3,),
-                  )
+                    child: ListTile(
+                      title: Text("All"),
+                      trailing: Image.asset('assets/icons/Polygon 1.png',scale: 3,),
+                    )
 
                 ),
               )
@@ -55,62 +85,62 @@ class SecondPage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.only(right: 15,left: 15,bottom: 8,top: 8),
             child: Container(
-              width: 300,
-              height: 90,
-              decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(10),
-                  boxShadow: [
-                    BoxShadow(
-                      blurRadius: 2,
-                      // color: Colors.grey
-                    )
-                  ]
-              ),
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  // ListTile(
-                  //   title: Text("Overspeed",style: TextStyle(color: Colors.black38,)),
-                  //   trailing: Text("2023-05-22",style: TextStyle(color: Colors.black38,)),
-                  //   subtitle: Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
-                  // )
-                  children: [
-                    Column(
-                    //  crossAxisAlignment: CrossAxisAlignment.stretch,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-
-                      children: [
-                        Text("Overspeed",style: TextStyle(color: Colors.black38),),
-                          Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
-                      ],
-                    ),
-                    Column(
-                      //  crossAxisAlignment: CrossAxisAlignment.stretch,
-                     mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-
-
-                      children: [
-                        Image.asset('assets/icons/notificationIcon.png',scale: 2.5,),
-                      ],
-                    ),
-                    Column(
-                      //  crossAxisAlignment: CrossAxisAlignment.stretch,
-                     mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.start,
-
-
-                      children: [
-                        Text("2023-06-05",style: TextStyle(color: Colors.black38,)),
-                        Text("10:06:47",style: TextStyle(color: Colors.black38,))
-                      ],
-                    ),
-                  ],
+                width: 300,
+                height: 90,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 2,
+                        // color: Colors.grey
+                      )
+                    ]
                 ),
-              )
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // ListTile(
+                    //   title: Text("Overspeed",style: TextStyle(color: Colors.black38,)),
+                    //   trailing: Text("2023-05-22",style: TextStyle(color: Colors.black38,)),
+                    //   subtitle: Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                    // )
+                    children: [
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          Text("Overspeed",style: TextStyle(color: Colors.black38),),
+                          Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Image.asset('assets/icons/notificationIcon.png',scale: 2.5,),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Text("2023-06-05",style: TextStyle(color: Colors.black38,)),
+                          Text("10:06:47",style: TextStyle(color: Colors.black38,))
+                        ],
+                      ),
+                    ],
+                  ),
+                )
             ),
           ),
 
@@ -426,110 +456,264 @@ class SecondPage extends StatelessWidget {
 
         ],
       );
-  }
-}
-class thirdPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 30.0,left: 30.0,top: 80.0,bottom: 80.0),
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(20),
-          boxShadow: [
-            BoxShadow(
-              color: Color(0xffe5e5e5),
-              blurRadius: 10,
-              offset: Offset(1,1),
-              spreadRadius: 5
-
-            )
-          ]
-
-        ),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(right: 4.0,left: 8.0,top: 20.0),
-              child: ListTile(
-                leading: Image.asset("assets/icons/notificationIcon.png",scale: 3,),
-                title: Text("Notification"),
-                trailing: SwitchScreen(),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: ListTile(
-                leading: Image.asset("assets/icons/vehicle.png",scale: 3,),
-                title: Text("Notification"),
-
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: ListTile(
-                leading: Image.asset("assets/icons/notificationIcon.png",scale: 3,),
-                title: Text("Notification"),
-
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: ListTile(
-                leading: Image.asset("assets/icons/notificationIcon.png",scale: 3,),
-                title: Text("Notification"),
-
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: ListTile(
-                leading: Image.asset("assets/icons/notificationIcon.png",scale: 3,),
-                title: Text("Notification"),
-
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: ListTile(
-                leading: Image.asset("assets/icons/notificationIcon.png",scale: 3,),
-                title: Text("Notification"),
-
-              ),
-            ),
-
-          ],
-        ),
 
 
-      ),
-    );
-  }
-}
-class fourthPage extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      child: Center(
-        child: Text(
-          'Second Page',
-          style: TextStyle(fontSize: 24, color: Colors.white),
-        ),
-      ),
-    );
+
   }
 }
 class fifthPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      child: Center(
-        child: Text(
-          'Second Page',
-          style: TextStyle(fontSize: 24, color: Colors.white),
+    return
+
+    SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.only(right: 30.0,left: 30.0,top: 80.0,bottom: 20.0),
+        child: Container(
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(20),
+              boxShadow: [
+                BoxShadow(
+                    color: Color(0xffe5e5e5),
+                    blurRadius: 10,
+                    offset: Offset(1,1),
+                    spreadRadius: 5
+
+                )
+              ]
+
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 40,),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Column(
+                        children: [
+                          Image.asset("assets/icons/bell-icon.png",scale: 3,),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text("Notification"),
+                      ],
+                    ),
+                    SizedBox(width: 90,),
+                    Column(
+                      children: [
+                        SwitchScreen(),
+                      ],
+                    )
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Column(
+                        children: [
+                          Image.asset("assets/icons/car-icon.png",scale: 3,),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text("Change Device Settings"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 17),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 15),
+                      child: Column(
+                        children: [
+                          Image.asset("assets/icons/command-icon.png",scale: 3,),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text("Command History"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Column(
+                        children: [
+                          Image.asset("assets/icons/geofence-icon.png",scale: 3,),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text("GeoFence Settings"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Column(
+                        children: [
+                          Image.asset("assets/icons/Group 22.png",scale: 3,),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text("Notifications Setings"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Column(
+                        children: [
+                          Image.asset("assets/icons/password-icon.png",scale: 3,),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text("Change Password"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Column(
+                        children: [
+                          Image.asset("assets/icons/time-zone-icon.png",scale: 3,),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text("Sync your Time Zone"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 22),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 20),
+                      child: Column(
+                        children: [
+                          // Image.asset("assets/icons/Group 25.png",scale: 3,),
+                          Image.asset('assets/icons/Group 36.png',scale: 3,)
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text("Change Language"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 22),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 22),
+                      child: Column(
+                        children: [
+                          Image.asset("assets/icons/cloudsnow.png",scale: 3,),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text("Maintenance"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 24),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(right: 28),
+                      child: Column(
+                        children: [
+                          Image.asset("assets/icons/Group 38.png",scale: 3,),
+                        ],
+                      ),
+                    ),
+                    Column(
+                      children: [
+                        Text("Assign Driver"),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 40,),
+
+
+
+
+
+            ],
+          ),
+
+
         ),
       ),
     );
