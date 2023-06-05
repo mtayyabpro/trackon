4050e7f154a50aa6d570e53c4add00cf597c1561
mtayyabpro <mtayyabpro@gmail.com>
2023-06-05 10:20:52 +0500

    update 05.06.23

--- lib/screens/track_record.dart
+++ lib/screens/track_record.dart
@@ -44,26 +44,15 @@ class SecondPage extends StatelessWidget {
                     title: Text("All"),
                     trailing: Image.asset('assets/icons/Polygon 1.png',scale: 3,),
                   )
-                  // TextField(
-                  //
-                  //
-                  //   decoration: InputDecoration(
-                  //     hintText: "All",
-                  //     suffixIcon: Image.asset('assets/icons/Polygon 1.png',scale: 3,),
-                  //     border: OutlineInputBorder()
-                  //
-                  //   ),
-                  //
-                  //
-                  //
-                  // ),
+
                 ),
               )
             ],
           ),
-          SizedBox(height: 15,),
+          SizedBox(height: 10,),
+
           Padding(
-            padding: const EdgeInsets.all(15.0),
+            padding: const EdgeInsets.only(right: 15,left: 15,bottom: 8,top: 8),
             child: Container(
               width: 300,
               height: 90,
@@ -77,23 +66,362 @@ class SecondPage extends StatelessWidget {
                     )
                   ]
               ),
-              child: Column(
-              //  crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  // ListTile(
+                  //   title: Text("Overspeed",style: TextStyle(color: Colors.black38,)),
+                  //   trailing: Text("2023-05-22",style: TextStyle(color: Colors.black38,)),
+                  //   subtitle: Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                  // )
+                  children: [
+                    Column(
+                    //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
 
-                children: [
-                 ListTile(
-                   title: Text("Overspeed",style: TextStyle(color: Colors.black38,)),
-                   trailing: Text("2023-05-22",style: TextStyle(color: Colors.black38,)),
+                      children: [
+                        Text("Overspeed",style: TextStyle(color: Colors.black38),),
+                          Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                      ],
+                    ),
+                    Column(
+                      //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                     mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
 
-                   subtitle: Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+
+                      children: [
+                        Image.asset('assets/icons/notificationIcon.png',scale: 2.5,),
+                      ],
+                    ),
+                    Column(
+                      //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                     mainAxisAlignment: MainAxisAlignment.center,
+                      crossAxisAlignment: CrossAxisAlignment.start,
 
 
-                 )
-                ],
+                      children: [
+                        Text("2023-06-05",style: TextStyle(color: Colors.black38,)),
+                        Text("10:06:47",style: TextStyle(color: Colors.black38,))
+                      ],
+                    ),
+                  ],
+                ),
               )
             ),
-          )
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 15,left: 15,bottom: 8,top: 8),
+            child: Container(
+                width: 300,
+                height: 90,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 2,
+                        // color: Colors.grey
+                      )
+                    ]
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // ListTile(
+                    //   title: Text("Overspeed",style: TextStyle(color: Colors.black38,)),
+                    //   trailing: Text("2023-05-22",style: TextStyle(color: Colors.black38,)),
+                    //   subtitle: Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                    // )
+                    children: [
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          Text("Overspeed",style: TextStyle(color: Colors.black38),),
+                          Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Image.asset('assets/icons/notificationIcon.png',scale: 2.5,),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Text("2023-06-05",style: TextStyle(color: Colors.black38,)),
+                          Text("10:06:47",style: TextStyle(color: Colors.black38,))
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 15,left: 15,bottom: 8,top: 8),
+            child: Container(
+                width: 300,
+                height: 90,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 2,
+                        // color: Colors.grey
+                      )
+                    ]
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // ListTile(
+                    //   title: Text("Overspeed",style: TextStyle(color: Colors.black38,)),
+                    //   trailing: Text("2023-05-22",style: TextStyle(color: Colors.black38,)),
+                    //   subtitle: Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                    // )
+                    children: [
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          Text("Overspeed",style: TextStyle(color: Colors.black38),),
+                          Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Image.asset('assets/icons/notificationIcon.png',scale: 2.5,),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Text("2023-06-05",style: TextStyle(color: Colors.black38,)),
+                          Text("10:06:47",style: TextStyle(color: Colors.black38,))
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 15,left: 15,bottom: 8,top: 8),
+            child: Container(
+                width: 300,
+                height: 90,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 2,
+                        // color: Colors.grey
+                      )
+                    ]
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // ListTile(
+                    //   title: Text("Overspeed",style: TextStyle(color: Colors.black38,)),
+                    //   trailing: Text("2023-05-22",style: TextStyle(color: Colors.black38,)),
+                    //   subtitle: Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                    // )
+                    children: [
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          Text("Overspeed",style: TextStyle(color: Colors.black38),),
+                          Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Image.asset('assets/icons/notificationIcon.png',scale: 2.5,),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Text("2023-06-05",style: TextStyle(color: Colors.black38,)),
+                          Text("10:06:47",style: TextStyle(color: Colors.black38,))
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 15,left: 15,bottom: 8,top: 8),
+            child: Container(
+                width: 300,
+                height: 90,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 2,
+                        // color: Colors.grey
+                      )
+                    ]
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // ListTile(
+                    //   title: Text("Overspeed",style: TextStyle(color: Colors.black38,)),
+                    //   trailing: Text("2023-05-22",style: TextStyle(color: Colors.black38,)),
+                    //   subtitle: Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                    // )
+                    children: [
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          Text("Overspeed",style: TextStyle(color: Colors.black38),),
+                          Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Image.asset('assets/icons/notificationIcon.png',scale: 2.5,),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Text("2023-06-05",style: TextStyle(color: Colors.black38,)),
+                          Text("10:06:47",style: TextStyle(color: Colors.black38,))
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(right: 15,left: 15,bottom: 8,top: 8),
+            child: Container(
+                width: 300,
+                height: 90,
+                decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(10),
+                    boxShadow: [
+                      BoxShadow(
+                        blurRadius: 2,
+                        // color: Colors.grey
+                      )
+                    ]
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    // ListTile(
+                    //   title: Text("Overspeed",style: TextStyle(color: Colors.black38,)),
+                    //   trailing: Text("2023-05-22",style: TextStyle(color: Colors.black38,)),
+                    //   subtitle: Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                    // )
+                    children: [
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+                        children: [
+                          Text("Overspeed",style: TextStyle(color: Colors.black38),),
+                          Text("demo Vehicle 1",style: TextStyle(color: Colors.black,fontSize: 20),),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Image.asset('assets/icons/notificationIcon.png',scale: 2.5,),
+                        ],
+                      ),
+                      Column(
+                        //  crossAxisAlignment: CrossAxisAlignment.stretch,
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+
+
+                        children: [
+                          Text("2023-06-05",style: TextStyle(color: Colors.black38,)),
+                          Text("10:06:47",style: TextStyle(color: Colors.black38,))
+                        ],
+                      ),
+                    ],
+                  ),
+                )
+            ),
+          ),
 
         ],
       );
